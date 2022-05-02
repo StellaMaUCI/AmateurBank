2a5851e4bddcf96c3a453536b39a036d9d3762f4
stellama <mashengwen0532@gmail.com>
2022-05-02 16:59:00 -0700

    Add up LayOut files and dirs

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import account
 app = Flask(__name__)
 
 
@@ -7,6 +7,8 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+app.register_blueprint(account.bp)
+app.add_url_rule('/', endpoint='index')
 
 if __name__ == '__main__':
     app.run()
